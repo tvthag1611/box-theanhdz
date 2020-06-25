c09dc2f1fd05545ac629c25a1e2140c16c57639a
huongnv <viethuong24092000@gmail.com>
2020-06-25 22:43:19 +0700

    json view

--- src/components/output/Output.js
+++ src/components/output/Output.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Output.css'
 import './Output.scss';
 import Switch from "react-switch";
+import JSONTreeComponent from "react-json-tree"
 
 export default function Output() {
     const [checked, setChecked] = useState(true);
@@ -10,7 +11,20 @@ export default function Output() {
         output: {
             pages: [
                 {
-                    textlines: [],
+                    textlines: [
+                        {
+                            "text": "HỌC VẤN",
+                            "confidence": 0.9711343947963079
+                        },
+                        {
+                            "text": "2014 - 2019",
+                            "confidence": 0.999746721478729
+                        },
+                        {
+                            "text": "HỌC VIỆN CÔNG NGHỆ BƯU CHÍNH VIỄN THÔNG",
+                            "confidence": 0.985105110407201
+                        }
+                    ],
                     rotation_angle: 0.15099197490648789,
                     height: 2530,
                     width: 1809,
@@ -34,6 +48,26 @@ export default function Output() {
         mlchain_version: "0.0.9i0"
     });
 
+    const theme = {
+        scheme: 'monokai',
+        base00: '#272822',
+        base01: '#383830',
+        base02: '#49483e',
+        base03: '#75715e',
+        base04: '#a59f85',
+        base05: '#f8f8f2',
+        base06: '#f5f4f1',
+        base07: '#f9f8f5',
+        base08: '#f92672',
+        base09: '#fd971f',
+        base0A: '#f4bf75',
+        base0B: '#a6e22e',
+        base0C: '#a1efe4',
+        base0D: '#66d9ef',
+        base0E: '#ae81ff',
+        base0F: '#cc6633'
+      };
+
     const handleChange = checked => {
         setChecked(checked);
     }
@@ -45,37 +79,46 @@ export default function Output() {
                     <i className="fa fa-download" aria-hidden="true"></i>
                     Download JSON
                 </button>
+                <div className="json-data">
+                    <JSONTreeComponent data={state} theme={theme} invertTheme={true}/>
+                </div>
+                
             </div>
         );
     }
 
+    const renderTable = () =>{
+        return state.output.pages[0].textlines.map((textline, index) => {
+            const {text, confidence} = textline;
+            return(
+                <tr key={index}>
+                    <td> {text} </td>
+                    <td>
+                        <div className="cover-box">{parseFloat(confidence).toFixed(3)}</div>
+                    </td>
+                </tr>
+            )
+        });
+    }
+
     const RowTextlines = () => {
-        return (
-            <table className="table">
-                <tr className="header">
-                    <th className="text">Text</th>
-                    <th className="confidence">Confidence</th>
-                </tr>
-                <tr>
-                    <td>adgasfhdsfaljgioshgfihsighushng</td>
-                    <td>
-                        <div className="cover-box">0.967</div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>adgasfhdsfaljgioshgfihsighushng</td>
-                    <td>
-                        <div className="cover-box">0.967</div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>adgasfhdsfaljgioshgfihsighushng</td>
-                    <td>
-                        <div className="cover-box">0.967</div>
-                    </td>
-                </tr>
+       
+            return(
+                <table className="table">
+                    <thead>
+                        <tr className="header">
+                            <th className="text">Text</th>
+                            <th className="confidence">Confidence</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderTable()}
+                    </tbody>
             </table>
-        );
+            )
+        
+            
+        
     }
 
     return (
